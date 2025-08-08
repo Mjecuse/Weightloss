e26e290142685810a59f38eb7a4cc62add09b554
Sarah J Watters <69488759+Mjecuse@users.noreply.github.com>
2025-08-08 15:26:41 -0400

    Change to add age. This changes the whole equation so that you get a more accurate result of how much to eat.

--- Weightloss.cpp
+++ Weightloss.cpp
@@ -19,7 +19,7 @@ void bmr() {
 	}
 	else if (sex == "male"){
 		float hb2 = 66.47 + (6.24 * weight) + (12.7 * inches) - (4.7 * age);
-		std::cout << "So you would need a minium of" << hb2 << " calories. ";
+		std::cout << "So you would need a minimum of " << hb2 << " calories. ";
 	}
 	else {
 	std::cout << "Unable to compute exact calories \n ";
@@ -30,26 +30,19 @@ void gbmr() {
 
 	if (sex == "female") {
 		float hb1 = 655.1 + (4.35 * goal) + (4.7 * inches) - (4.7 * age);
-		std::cout << "So you would need a minium of" << hb1 << " calories. ";
+		std::cout << "So you would need a minium of  \n" << hb1 << " calories. ";
 	}
 	else if (sex == "male") {
 		float hb2 = 66.47 + (6.24 * goal) + (12.7 * inches) - (4.7 * age);
-		std::cout << "So you would need a minium of" << hb2 << " calories. ";
+		std::cout << "So you would need a minimum of \n" << hb2 << " calories. ";
 	}
 	else {
 		std::cout << "Unable to compute exact calories \n ";
 	}
 }
 
-
-
-
-
-
 int main()
 {
-
-
 
     std::cout << "What is your name?\n";
 	std::cin >> name;
@@ -60,25 +53,13 @@ int main()
 	std::cin >> weight;
 	std::cout << "What is your height in inches?\n ";
 	std::cin >> inches;
+	std::cout << "what is your age?";
+	std::cin >> age;
 	std::cout << " So " << name << ", you weigh " << weight << " pounds. \n";
 	bmr();
 	std::cout << "What is your goal weight?\n";
 	std::cin >> goal;
-	std::cout<< "  So your goal weight is" << goal << " pounds. Realisticly, how many weeks do you want to take to reach your goal? Remember... you can't lose more than 2 pounds a week.\n";
 	gbmr();
-	
+		std::cout<< "  So your goal weight is" << goal << " pounds. Realisticly, how many weeks do you want to take to reach your goal? Remember... you can't lose more than 2 pounds a week.\n";
 	std::cin >> weeks;
-
-
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
