9d360bc2c3f902c62cb7ac641e0ca968545a0477
Sarah J Watters <69488759+Mjecuse@users.noreply.github.com>
2025-08-08 13:10:19 -0400

    Work in progress. Teaching myself C++

--- Weightloss.cpp
+++ Weightloss.cpp
@@ -3,25 +3,45 @@
 
 #include <iostream>
 #include <string>
-
-void bmr() {
-	int age;
+int age;
 	float weight;
 	std::string name;
 	float goal;
 	int inches;
 	int weeks;
 	std::string sex;
+void bmr() {
+	
+	
 	if (sex == "female") {
 		float hb1 = 655.1 + (4.35 * weight) + (4.7 * inches) - (4.7 * age);
+		std::cout << "So you would need a minium of" << hb1 << " calories. ";
 	}
 	else if (sex == "male"){
 		float hb2 = 66.47 + (6.24 * weight) + (12.7 * inches) - (4.7 * age);
+		std::cout << "So you would need a minium of" << hb2 << " calories. ";
 	}
 	else {
-	std::cout << "Unable to compute exact calories";
+	std::cout << "Unable to compute exact calories \n ";
 	}
 }
+void gbmr() {
+
+
+	if (sex == "female") {
+		float hb1 = 655.1 + (4.35 * goal) + (4.7 * inches) - (4.7 * age);
+		std::cout << "So you would need a minium of" << hb1 << " calories. ";
+	}
+	else if (sex == "male") {
+		float hb2 = 66.47 + (6.24 * goal) + (12.7 * inches) - (4.7 * age);
+		std::cout << "So you would need a minium of" << hb2 << " calories. ";
+	}
+	else {
+		std::cout << "Unable to compute exact calories \n ";
+	}
+}
+
+
 
 
 
@@ -33,17 +53,20 @@ int main()
 
     std::cout << "What is your name?\n";
 	std::cin >> name;
-	std::cout << "Hello " << name << "I am going to ask you some important questions. Please answer them honestly";
+	std::cout << "Hello " << name << ". I am going to ask you some important questions. Please answer them honestly\n";
 	std::cout << "Are you Male or Female?";
 	std::cin >> sex;
     std::cout << "What is your weight?\n";
 	std::cin >> weight;
 	std::cout << "What is your height in inches?\n ";
 	std::cin >> inches;
+	std::cout << " So " << name << ", you weigh " << weight << " pounds. \n";
 	bmr();
-	std::cout << "Thank you, so " << name << ", you weigh " << weight << " pounds. Can you tell me what is your goal weight? \n";
+	std::cout << "What is your goal weight?\n";
 	std::cin >> goal;
-	std::cout<< "So your goal weight is" << goal << " pounds. Realisticly, how many weeks do you want to take to reach your goal? Remember... you can't lose more than 2 pounds a week.\n";
+	std::cout<< "  So your goal weight is" << goal << " pounds. Realisticly, how many weeks do you want to take to reach your goal? Remember... you can't lose more than 2 pounds a week.\n";
+	gbmr();
+	
 	std::cin >> weeks;
 
 
